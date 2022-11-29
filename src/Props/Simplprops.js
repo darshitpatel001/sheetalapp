8b2dfd6603f75d881d12a4d03434c8b4afbc4ac2
darshitpatel001 <darshitakbari9999@gmail.com>
2022-11-29 18:39:33 +0600

    data

--- src/Props/Simplprops.js
+++ src/Props/Simplprops.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function Simplprops(first) {
   return (
-    <div >
-    <div class="card" style="width:400px">
-    <img class="card-img-top" src={first.record.image}/>
-    <div class="card-body">
-      <h4 class="card-title">{first.record.id}</h4>
-      <h4 class="card-title">{first.record.title}</h4>
-      <h4 class="card-title">{first.record.price}</h4>
-      <h4 class="card-title">{first.record.description}</h4>
-      <p class="card-text">Some example text.</p>
-      <a href="#" class="btn btn-primary">See Profile</a>
+    <div>
+      <div class="card">
+        <img class="card-img-top" src={first.record.image} />
+        <div class="card-body">
+          <h4 class="card-title">{first.record.id}</h4>
+          <h4 class="card-title">{first.record.title}</h4>
+          <h4 class="card-title">{first.record.price}</h4>
+          <h4 class="card-title">{first.record.description}</h4>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-  )
+  );
 }
