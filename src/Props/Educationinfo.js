d2fd47edb4d14a433d3a9889b0ab5af25926c7e1
darshitpatel001 <darshitakbari9999@gmail.com>
2022-11-30 16:22:28 +0600

    data

--- src/Props/Educationinfo.js
+++ src/Props/Educationinfo.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function Educationinfo(props) {
   return (
     <div>
-      {props.data.map((value,index) => {
+      {props.data.map((value, index) => {
         return (
-          <div>{value.firstname}{value.lastname}<button onClick={() => {props.editmy(index);}}>Click</button>
+          <div>
+            <h4>{value.firstname} {value.lastname}<button onClick={() => {props.editmy(index);}}>Edit </button>
+              <button onClick={() => {props.remove(index);}}>Remove</button>
+            </h4>
           </div>
         );
       })}
--- src/Props/Eductiontodo.js
+++ src/Props/Eductiontodo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Educationinfo from "./Educationinfo"
+import "./Educatio.css";
 import { useState } from "react";
 
 export default function Eductiontodo() {
@@ -9,6 +10,7 @@ export default function Eductiontodo() {
   });
 
   const [first, setfirst] = useState([]);
+  const [index, setindex] = useState(-1);
 
   const Handler = (e) => {
     setdata({ ...data, [e.target.name]: e.target.value });
@@ -16,26 +18,47 @@ export default function Eductiontodo() {
 
   const edit = (index) => {
     let myobj = first[index];
-    setfirst(myobj);
-
+    setdata(myobj);
+    setindex(index);
   };
+  
+  const mydelete = (index) => {
+    const myArray = [...first];
+    myArray.splice(index,1);
 
+    setfirst(myArray);
+  }
   const mySubmit = (e) => {
     e.preventDefault();
+    if (index >=0)
+    {
+      let a = [...first];
+      a[index].firstname = data.firstname;
+      a[index].lastname = data.lastname;
+
+      setfirst(a);
+    }
+else
+{
     let dd = [...first];
     dd.push(data);
     setfirst(dd);
+    setindex(index);
+}
 
   };
   return (
-    <div>
-      <form onSubmit={mySubmit}>
-        <label> FirstName : </label>
-        <input type="text" name="firstname"  onChange={Handler} />
-        <input type="text" name="lastname" onChange={Handler} />
-    
-        <input type="submit" value="submit" />
-        <Educationinfo data={first} editmy={edit}></Educationinfo>
+    <div class="container">
+      <form  id="contact" onSubmit={mySubmit}>
+       <h6> <label> First Name :</label></h6>
+        <input type="text" name="firstname" value={data.firstname}  onChange={Handler} />
+        <h6> <label> Last Name :</label></h6>
+        <input type="text" name="lastname" value={data.lastname} onChange={Handler} />
+
+        
+        <Educationinfo data={first} editmy={edit} remove={mydelete}></Educationinfo>
+        <input className="button" type="submit" value="submit" />
+        
       </form>
     </div>
   );
