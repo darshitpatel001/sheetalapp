30f93d7ae46d4643298e1c18d46b0af8556f50e4
darshitpatel001 <darshitakbari9999@gmail.com>
2022-12-03 19:32:37 +0600

    dats

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 import { Formik, Form, Field } from "formik";
@@ -7,32 +6,31 @@ import { Formik, Form, Field } from "formik";
 export default function () {
   
   return (
-    <div>
+    <div class="w-50 mx-auto">
+      <h2 class="text-center">Login Form</h2>
       <Formik
         initialValues={{
           email: "",
           password: "",
         }}
         onSubmit={(e) => {
-          axios.post("http://localhost:4000/api-docs/#/default/authenticate",e)
+          axios.post("http://localhost:4000/accounts/authenticate",e)
             .then(y=> {
               console.log(y.data);
               toast("Login is Successful");
 
-            }).catch(
-              () =>{
-                toast("Login is Faild");
-
-            });
-        }}
+            }).catch(() =>{
+              toast("Login is Faild");
+            })
+          }}
       >
-        <Form class="container">
-          <label htmlFor=""> Email : </label>
-          <Field type="text" name="email"/><br/>
-          <label htmlFor=""> Password : </label>
-          <Field type="text"  name="password"  /><br/>
+        <Form >
+          <label htmlFor="" class="form-label"> Email : </label>
+          <Field type="email" class="form-control" name="email"/><br/>
+          <label htmlFor=""  class="form-label"> Password : </label>
+          <Field type="password" class="form-control" name="password"  /><br/>
 
-          <input type="submit" value="submit" />
+          <input type="submit" class="form-control btn btn-outline-primary" value="Sign-in" />
         </Form>
       </Formik>
     </div>
