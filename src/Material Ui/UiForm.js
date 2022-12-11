64b47ac538e6e1fdd79870eb9c82c49fc7df036b
darshitpatel001 <darshitakbari9999@gmail.com>
2022-12-11 18:45:47 +0600

    dd

--- src/Material Ui/UiForm.js
+++ src/Material Ui/UiForm.js
@@ -1,85 +1,106 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
-import './UiForm.css'
+import axios from "axios";
+import "./UiForm.css";
+import { Formik, Form } from "formik";
 
 export default function UiForm() {
-    const [data, setdata] = useState({
-        fname: "",
-        mname: "",
-        lname: "",
-        email: "",
-        password:"",
-        city: "",
-      });
-      const Handler = (e) => {
-        setdata({ ...data, [e.target.name]: e.target.value});
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(data);
-      };
-      
-      return (
-        <div class="w-50 mx-auto">
-          <form onSubmit={handleSubmit}>
-             <h2>Registation Form</h2>
-            <TextField
-              label="First Name"
-              variant="standard"
-              name="fname"
-              onChange={Handler}
-            />
-            <br />
-            <TextField
-              label="Middle Name"
-              variant="standard"
-              name="mname"
-              onChange={Handler}
-            />
-            <br />
-            <TextField
-              label="Last Name"
-              variant="standard"
-              name="lname"
-              onChange={Handler}
-            />
-            <br />
-            <TextField
-              label="Password"
-              variant="standard"
-               type="password"
-               autoComplete="current-password"
-              name="password"
-              onChange={Handler}
-            /><br/>
+  const displayData = () => {
+    let data = localStorage.getItem("Registation");
+    let d = JSON.parse(data);
 
-            <TextField
-              id="outlined-select-currency-native"
-              variant="standard"
-              select
-              label="select"
-              SelectProps={{
-                native: true,
-              }}
-              name="city"
-              onChange={Handler}
-              helperText="Please select your currency"
-            >
-                <option></option>
-              <option>Surat</option>
-              <option>Vadodara</option>
-              <option>Bhavnagr</option>
-              <option>Rajkot</option>
-            </TextField>
-            <br />
-            <input
-              class="form-control btn btn-outline-primary"
-              type="submit"
-              value="Save"
-            />
-          </form>
-        </div>
-      );
-    }
+    axios
+      .get("http://localhost:4000/account", {
+        headers: {
+          Authorization: "Bearer " + d?.jwtToken,
+        },
+      })
+      .then((y) => {
+        console.log(y.data);
+      })
+      .catch((y) => {
+        console.log(y);
+      });
+  };
+
+  return (
+    <div class="w-50 mx-auto">
+      <Formik
+        initialValues={{
+          fname: "",
+          mname: "",
+          lname: "",
+          email: "",
+          password: "",
+          city: "",
+        }}
+        onSubmit={(e) => {
+          axios
+            .post("http://localhost:4000/accounts/authenticate", e)
+            .then((y) => {
+              console.log(y.data);
+              localStorage.setItem("Registation", JSON.stringify(y.data));
+            });
+        }}
+      >
+        <Form>
+          <h2>Registation Form</h2>
+          <TextField 
+            label="First Name" 
+            variant="standard" 
+            name="fname" />
+          <br />
+          <TextField 
+            label="Middle Name" 
+            variant="standard" 
+            name="mname" />
+          <br />
+          <TextField  
+            label="Last Name" 
+            variant="standard" 
+            name="lname" />
+          <br />
+          <TextField 
+            label="email" 
+            variant="standard" 
+            name="email" />
+          <br />
+          <TextField
+            label="Password"
+            variant="standard"
+            type="password"
+            autoComplete="current-password"
+            name="password"
+          />
+          <br />
+
+          <TextField
+            id="outlined-select-currency-native"
+            variant="standard"
+            select
+            label="select"
+            SelectProps={{
+              native: true,
+            }}
+            name="city"
+            helperText="Please select your currency"
+          >
+            <option></option>
+            <option>Surat</option>
+            <option>Vadodara</option>
+            <option>Bhavnagr</option>
+            <option>Rajkot</option>
+          </TextField>
+          <br />
+          <input
+            class="form-control btn btn-outline-primary"
+            type="submit"
+            value="Save"
+            onClick={displayData}
+          />
+        </Form>
+      </Formik>
+    </div>
+  );
+}
